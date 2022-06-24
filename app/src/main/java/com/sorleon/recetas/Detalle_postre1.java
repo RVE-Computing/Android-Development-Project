f66fe91bf4f80f35baf5109963c8af1c254ea405
Alberto Soria <soria.deleon@gmail.com>
2022-06-25 00:39:52 +0200

    Añade Pantalla Detalles Postre
    Se añade la pantalla de Detalle Postre

---
+++ app/src/main/java/com/sorleon/recetas/Detalle_postre1.java
@@ -0,0 +1,16 @@
+package com.sorleon.recetas;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Detalle_postre1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_postre1);
+    }
+
+
+}
