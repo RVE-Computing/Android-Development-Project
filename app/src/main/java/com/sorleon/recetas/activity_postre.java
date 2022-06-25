192325e947ada9de25416bac58aeaecd67b43fea
Elena Piaggio <elenapiaggio@gmail.com>
2022-06-25 10:46:38 +0200

    UPDATE activity_postre

--- app/src/main/java/com/sorleon/recetas/activity_postre.java
+++ app/src/main/java/com/sorleon/recetas/activity_postre.java
@@ -1,44 +1,14 @@
 package com.sorleon.recetas;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class activity_postre {
+import android.os.Bundle;
+
+public class activity_postre extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_postre);
+        setContentView(R.layout.activity_postre2);
     }
-
-    private void setContentView(int activity_postre) {
-
-        //La idea puede ser 1 pantalla por postre o 1 pantalla de contenido variable en funcion del botón
-
-        //Metodo del boton para ir a macedonia
-        public void macedonia(View view){
-            Intent macedonia = new Intent(this, Detalle_postre1.class);
-            startActivity(macedonia);
-        }
-
-        //Metodo del boton para ir a Tarta de queso
-        public void tartaqueso(View view){
-            Intent tartaqueso = new Intent(this, Detalle_postre1.class);
-            startActivity(tartaqueso);
-        }
-
-        //Metodo del boton para ir a Churros
-        public void churros(View view){
-            Intent churros = new Intent(this, Detalle_postre1.class);
-            startActivity(churros);
-        }
-    }
-
-
-
-
 }
-
-
-
